2d3c275ee9ba7d4831ca9269909d3095222f6580
mansoor <t.alyamani95@gmail.com>
2025-07-09 17:37:11 +0700

    Input Array Elements

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -21,4 +21,12 @@ void input()
         }
     }
 
+    cout << "\n==========================================\n";
+    cout << "         Masukkan Elemen Array            \n";
+    cout << "==========================================\n";
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
 }
